1cd4de7c14de30eefa9ad6d9b8fdc1bb8bdd8c60
Oladoyin Ayodeji Felix <oladoyinayodeji@gmail.com>
2021-11-24 11:51:28 +0100

    packages installation

--- attributes_methods.py
+++ attributes_methods.py
@@ -1,14 +1,21 @@
 # OOP Attributes and Methods
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
+# timmy = Turtle()
 
-timmy.shape('turtle')
-timmy.color('green')
-timmy.forward(distance=100)
+# timmy.shape('turtle')
+# timmy.color('green')
+# timmy.forward(distance=100)
 
-my_screen = Screen()
+# my_screen = Screen()
 
-print(my_screen.canvheight)
+# print(my_screen.canvheight)
 
-my_screen.exitonclick()
+# my_screen.exitonclick()
+
+#=========================
+# PyPi.org
+
+# first install the package e.g prettytabe
+# pip install PrettyTable
+import prettytable
