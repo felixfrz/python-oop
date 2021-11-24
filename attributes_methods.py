c7e8eea1e9b6f041b05709b31c90e710a0f8e751
Oladoyin Ayodeji Felix <oladoyinayodeji@gmail.com>
2021-11-24 12:06:02 +0100

    Practicing with OOP using PrettyTable Module

--- attributes_methods.py
+++ attributes_methods.py
@@ -1,14 +1,27 @@
 # OOP Attributes and Methods
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
+# timmy = Turtle()
 
-timmy.shape('turtle')
-timmy.color('green')
-timmy.forward(distance=100)
+# timmy.shape('turtle')
+# timmy.color('green')
+# timmy.forward(distance=100)
 
-my_screen = Screen()
+# my_screen = Screen()
 
-print(my_screen.canvheight)
+# print(my_screen.canvheight)
 
-my_screen.exitonclick()
+# my_screen.exitonclick()
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column('States In Nigeria', ['Lagos', 'Ondo', 'Rivers', 'Cross Rivers'])
+table.add_column('Capital', ['Ikeja', 'Akure', 'Port Harcourt', 'Calabar'])
+
+# Attribute
+table.align = 'l' #left aligment
+
+print(table)
